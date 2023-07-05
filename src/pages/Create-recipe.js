820e428ccd689000178861a233ebf0f30cab1aa9
prachip1 <prachip1908@gmail.com>
2023-07-05 21:09:29 +0530

    second upload

--- src/pages/Create-recipe.js
+++ src/pages/Create-recipe.js
@@ -1,10 +1,84 @@
-import React from "react";
+import React,{useState}from "react";
+import axios from "axios";
+import { useGetUserID } from "../hooks/useGetUserID";
+import { useNavigate } from "react-router-dom";
+
+
+
 
 export const CreateRecipes =()=>{
+    const userID = useGetUserID();
+    const navigate = useNavigate();
+
+    const [recipe, setRecipe] = useState({
+        name:"",
+        ingredients:[],
+        instructions:"",
+        imageURL:'',
+        cookingTime:0,
+        userOwner: userID,
+    });
+
+    const handleChange = (event) =>{
+        const { name, value } = event.target;
+        setRecipe({...recipe, [name]:value});
+
+    };
+
+    const handleIngredientChange =(event, idx)=>{
+        const { value } = event.target;
+        const ingredients = recipe.ingredients;
+        ingredients[idx] = value;
+        setRecipe({...recipe, ingredients});
+    }
+
+    const addIngredient = () =>{
+        setRecipe({...recipe, ingredients:[...recipe.ingredients, ""]});
+    };
+   
+    const onSubmit = async (event) =>{
+        event.preventDefault();
+        try{
+            await axios.post("http://localhost:3001/recipes", recipe);
+            alert("recipe created");
+            navigate("/");
+
+
+        }catch(err){
+         console.error(err);
+        }
+    }
+
+
+
 
     return(
-     <div>
-    CreateRecipes
+     <div className="create-recipe">
+     <h5>Create Recipe</h5>
+     <form onSubmit={onSubmit}>
+        <label htmlFor="">Recipe Name:</label>
+        <input type="text" id="name" name="name" onChange={handleChange} />
+        <label htmlFor="ingredients">Ingredients</label>
+        {recipe.ingredients.map((ingredient, idx) => (
+            <input 
+            key ={idx} 
+            type="text" 
+            name="ingredients" 
+            value={ingredient}
+            onChange={(event) =>{handleIngredientChange(event,idx)}}
+            
+            />
+        ))}
+
+        <button onClick={addIngredient} type="button"> + </button>
+        <label htmlFor="instructions">Instructions</label>
+        <textarea name="instructions" id="instructions" onChange={handleChange} ></textarea>
+        <label htmlFor="imageURL">Image URL</label>
+        <input type="url" id="imageURL" name="imageURL" onChange={handleChange} />
+        <label htmlFor="cookingTime">Cooking Time(minutes)</label>
+        <input type="number" id="cookingTime" name="cookingTime" onChange={handleChange}  />
+        <button type="submit"> submit </button>
+     </form>
      </div>
 
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,57 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./home.css";
+import axios from "axios";
 
-export const Home =()=>{
+export const Home = () => {
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => 
+    {
+     const fetchRecipe = async () =>
+     {
+        try{
+              const response = await axios.get("http://localhost:3001/recipes");
+              setRecipes(response.data) 
+              console.log(response.data)
+    
+    
+            }catch(err){
+             console.error(err);
+            }
+
+           
+    }
+        fetchRecipe();
+     }, [])
+    
+
+
 
     return(
-     <div>
-     <h2>Home</h2>
+     <div className="homecss">
+     <div className="headercss">
+     <h2>Recipes Main Page</h2>
+    
+    </div>
+     <div className="homecontent">
+     <ul>
+        {recipes.map((recipe) =>(
+            <li key={recipe._id}>
+              <div>
+                <h3>{recipe.name}</h3>
+              </div>
+              <div>
+                <p>{recipe.instructions}</p>
+              </div>
+              <img src={recipe.imageURL} alt={recipe.name} />
+              <p>Cooking Time: {recipe.cookingTime} (minutes)
+              </p>
+            </li>
+        ))}
+      </ul>
+
+     </div>
+   
      </div>
 
     );
